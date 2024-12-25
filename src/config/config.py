221c342473b84c586f95015659270a78d99775f3
Ansh-info <anshkumar.info@gmail.com>
2024-12-25 01:21:51 +0100

    Add: Finds the project root by going up one directory

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,18 @@
-# config.py
+import json
+import logging
+import os
+import sys
+import time
+from datetime import datetime
+from pathlib import Path
 from typing import Dict, List
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+    print(f"Added to Python path: {project_root}")
+
 
 # GCP Configuration
 GCP_CONFIG = {
@@ -9,6 +22,7 @@ GCP_CONFIG = {
     "DATASET_NAME": "stock_market",
 }
 
+
 # Stock Configuration
 STOCK_CONFIGS = {
     "AMZN": {
@@ -16,17 +30,56 @@ STOCK_CONFIGS = {
         "table_name": "amazon_stock",
         "interval": "5min",
     },
-    "GOOGL": {
-        "api_key": "J30SRXLUMQK4EW8Y",  # Replace with actual API key
-        "table_name": "google_stock",
+    "TSLA": {
+        "api_key": "J30SRXLUMQK4EW8Y",
+        "table_name": "tesla_stock",
+        "interval": "5min",
+    },
+    "PFE": {
+        "api_key": "2RW2VSDAMEKSTSSV",
+        "table_name": "pfizer_stock",
+        "interval": "5min",
+    },
+    "JPM": {
+        "api_key": "2RW2VSDAMEKSTSSV",
+        "table_name": "jpmorgan_stock",
+        "interval": "5min",
+    },
+    "IBM": {
+        "api_key": "4R98MITURZRRFCAW",
+        "table_name": "ibm_stock",
+        "interval": "5min",
+    },
+    "XOM": {
+        "api_key": "2RW2VSDAMEKSTSSV",
+        "table_name": "exxonmobil_stock",
+        "interval": "5min",
+    },
+    "KO": {
+        "api_key": "2RW2VSDAMEKSTSSV",
+        "table_name": "cocacola_stock",
+        "interval": "5min",
+    },
+    "AAPL": {
+        "api_key": "4R98MITURZRRFCAW",
+        "table_name": "apple_stock",
         "interval": "5min",
     },
     "MSFT": {
-        "api_key": "J30SRXLUMQK4EW8Y",  # Replace with actual API key
+        "api_key": "DYDIKRBOWNRQ9CUA",
         "table_name": "microsoft_stock",
         "interval": "5min",
     },
-    # Add more stocks as needed
+    "GOOGL": {
+        "api_key": "DYDIKRBOWNRQ9CUA",
+        "table_name": "google_stock",
+        "interval": "5min",
+    },
+    "NVDA": {
+        "api_key": "DYDIKRBOWNRQ9CUA",
+        "table_name": "nvidia_stock",
+        "interval": "5min",
+    },
 }
 
 
